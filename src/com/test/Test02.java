c52bf454ab5cf7687f4cb608c7553a78c24f2a24
TWM <15152806231@163.com>
2019-08-29 15:01:25 +0800

    常见异常

---
+++ src/com/test/Test02.java
@@ -0,0 +1,22 @@
+package com.test;
+/**
+ * 常见的异常
+ * @author Administrator
+ *
+ */
+public class Test02 {
+
+	public static void main(String[] args) {
+		/**
+		 * ParseException 解析异常，日期字符串转换为日期对象的时候，有可能抛出的异常
+		 * OutOfIndexException 数组下标越界异常
+		 * OutOfMemoryError 内存不足
+		 * ClassCastException 类型转换异常
+		 * ArithmeticException 除数为零
+		 * NullPointerException 空指针异常
+		 * FileNotFoundException 文件不存在异常
+		 */
+
+	}
+
+}
