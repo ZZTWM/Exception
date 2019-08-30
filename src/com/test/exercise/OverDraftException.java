314f1d443e3d3ba23087b0770721b64cdb70d8a8
TWM <15152806231@163.com>
2019-08-30 08:55:20 +0800

    异常练习01

---
+++ src/com/test/exercise/OverDraftException.java
@@ -0,0 +1,13 @@
+package com.test.exercise;
+
+public class OverDraftException extends Exception {
+	private double deficit;
+	public OverDraftException(String msg,double deficit){
+		super(msg);
+		this.deficit = deficit;
+	}
+	public double getDeficit() {
+		return deficit;
+	}
+	
+}
